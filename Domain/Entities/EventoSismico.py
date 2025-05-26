583989c1e8ab9b086fd7dfff6de2573f78a4feeb
Sergio Garzon <sggarzonutn@gmail.com>
2025-05-25 22:00:17 -0300

    Updated project again

--- Domain/Entities/EventoSismico.py
+++ Domain/Entities/EventoSismico.py
@@ -1,10 +1,16 @@
 from Domain.Entities.Estado import Estado
+from Domain.Entities.AlcanceSismo import AlcanceSismo
+from Domain.Entities.OrigenDeGeneracion import OrigenDeGeneracion
+from Domain.Entities.ClasificacionSismo import ClasificacionSismo
 from datetime import datetime
 
 class EventoSismico:
     
     def __init__(self, fecha_hora_fin, fecha_hora_ocurrencia, latitud_epicentro, latitud_hipocentro, 
-                 longitud_epicentro, longitud_hipocentro, valor_magnitud, ambito, nombre_estado):
+                 longitud_epicentro, longitud_hipocentro, valor_magnitud, ambito, nombre_estado,
+                 nombre_alcance, descripcion_alcance,
+                 nombre_origen_generacion, descripcion_origen_generacion,
+                 nombre_clasificacion_sismo, kilometro_profundidad_desde, kilometro_profundidad_hasta):
         self.fecha_hora_fin = datetime.strptime(fecha_hora_fin, "%Y-%m-%d %H:%M:%S")
         self.fecha_hora_ocurrencia = datetime.strptime(fecha_hora_ocurrencia, "%Y-%m-%d %H:%M:%S")
         self.latitud_epicentro = latitud_epicentro
@@ -13,6 +19,9 @@ class EventoSismico:
         self.longitud_hipocentro = longitud_hipocentro
         self.valor_magnitud = valor_magnitud
         self.estado_actual = Estado(ambito, nombre_estado)
+        self.alcance_sismo = AlcanceSismo(nombre_alcance, descripcion_alcance)
+        self.generacion_sismo = OrigenDeGeneracion(nombre_origen_generacion, descripcion_origen_generacion)
+        self.clasificacion_sismo = ClasificacionSismo(nombre_clasificacion_sismo, kilometro_profundidad_desde, kilometro_profundidad_hasta)
     
     def get_fecha_hora_ocurrencia(self):
         return self.fecha_hora_ocurrencia
@@ -34,6 +43,10 @@ class EventoSismico:
     
     def get_valor_magnitud(self):
         return self.valor_magnitud
+    
+    # DATOS RESTANTES PARA BUSCAR TODOS
+    def get_datos_restante(self):
+        return AlcanceSismo.get_nombre() + OrigenDeGeneracion.get_nombre() + ClasificacionSismo.get_nombre()
     
     def es_pendiente_revision(self):
         valor = False
